b56e0fa61f33be6a02c90b470c184ff5653e2128
Michael Eisemann <me@michaeleisemann.com>
2022-12-04 00:19:44 -0500

    day04: remove extraneous calls to Array()

--- day04/main.swift
+++ day04/main.swift
@@ -12,8 +12,8 @@ var totalAnySubRanges: Int = 0
 
 while let line = readLine() {
     let sectionRanges = line.components(separatedBy: ",").map { $0.components(separatedBy: "-").map { Int($0) ?? -1 } }
-    let rangeOne = Set(Array(sectionRanges[0][0]...sectionRanges[0][1]))
-    let rangeTwo = Set(Array(sectionRanges[1][0]...sectionRanges[1][1]))
+    let rangeOne = Set(sectionRanges[0][0]...sectionRanges[0][1])
+    let rangeTwo = Set(sectionRanges[1][0]...sectionRanges[1][1])
     if rangeOne.isSubset(of: rangeTwo) || rangeTwo.isSubset(of: rangeOne) {
         totalFullSubRanges += 1
     }
