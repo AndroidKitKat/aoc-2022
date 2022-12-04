49da7b6a4781b803dbdad016ff69bc45519f0806
Michael Eisemann <me@michaeleisemann.com>
2022-12-04 00:42:30 -0500

    day01: SwiftLinted

--- day01/main.swift
+++ day01/main.swift
@@ -14,7 +14,7 @@ while let line = readLine(strippingNewline: true) {
         guard let intLine = Int(line) else { continue }
         calories.append(intLine)
     } else {
-        let totalCalories:Int = calories.reduce(0, +)
+        let totalCalories: Int = calories.reduce(0, +)
         calorieCounts.append(totalCalories)
         calories.removeAll()
     }
@@ -23,4 +23,3 @@ while let line = readLine(strippingNewline: true) {
 calorieCounts.sort(by: >)
 print(calorieCounts[0])
 print(calorieCounts[0] + calorieCounts[1] + calorieCounts[2])
-
